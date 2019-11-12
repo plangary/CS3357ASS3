c07758b73cc0ba417d4cf758e42d5a1b26759b31
Pouya Langary <plangary@uwo.ca>
2019-11-12 14:01:31 -0500

    update

--- UDP_Client.py
+++ UDP_Client.py
@@ -4,24 +4,77 @@ import struct
 import sys
 import hashlib
 
+
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
+
+ack=0
+received_ack=-1
 
 print("UDP target IP:", UDP_IP)
 print("UDP target port:", UDP_PORT)
 
-#Create the Checksum
-values = (0,0,b'TestData')
-UDP_Data = struct.Struct('I I 8s')
-packed_data = UDP_Data.pack(*values)
-chksum =  bytes(hashlib.md5(packed_data).hexdigest(), encoding="UTF-8")
+data_to_send = ('NCC-1701','NCC-1422','NCC-1017')
 
-#Build the UDP Packet
-values = (0,0,b'TestData',chksum)
-UDP_Packet_Data = struct.Struct('I I 8s 32s')
-UDP_Packet = UDP_Packet_Data.pack(*values)
+for data in data_to_send:
 
-#Send the UDP Packet
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
-sock.sendto(UDP_Packet, (UDP_IP, UDP_PORT))
+    data_bytes = data.encode("utf-8")
+    
+    #Create the Checksum
+    values = (0,seq,data_bytes)
+    UDP_Data = struct.Struct('I I 8s')
+    packed_data = UDP_Data.pack(*values)
+    chksum =  bytes(hashlib.md5(packed_data).hexdigest(), encoding="UTF-8")
+
+    #Build the UDP Packet
+    values = (0,seq,bytes_data,chksum)
+    UDP_Packet_Data = struct.Struct('I I 8s 32s')
+    UDP_Packet = UDP_Packet_Data.pack(*values)
+
+    #Send the UDP Packet
+    sock = socket.socket(socket.AF_INET, # Internet
+                         socket.SOCK_DGRAM) # UDP
+    sock.sendto(UDP_Packet, (UDP_IP, UDP_PORT))
+    
+    print("Packet sent:", values)
+
+    resp, addr = sock.recvfrom(4096)
+    packet_received = unpacker.unpack(resp)
+    print("Packet received:",packet_received)
+
+    received_ack =packet_received[0]
+
+    while received_ack != ack:
+        print("Packet Corrupted: ACKs do not match")
+
+        sock.sendto(UDP_Packet,(UDP_IP,UDP_PORT))
+
+        print("Resending Packet:", UDP_Packet)
+
+        resp, addr = sock.recvfrom(4096)
+        packet_received = unpacket.unpack(resp)
+        receievd_ack = packet_received[0]
+        print("Packet Receieved: ",resp)
+
+
+    if seq==1:
+        seq=0
+    else:
+        seq =1
+
+    if ack==1:
+        ack=0
+    else:
+        ack=1
+
+
+sock.close()
+        
+
+
+
+        
+    
+
+
+    
--- UDP_Server.py
+++ UDP_Server.py
@@ -20,13 +20,37 @@ while True:
     UDP_Packet = unpacker.unpack(data)
     print("received from:", addr)
     print("received message:", UDP_Packet)
+    
+
+    
     #Create the Checksum for comparison
     values = (UDP_Packet[0],UDP_Packet[1],UDP_Packet[2])
     packer = struct.Struct('I I 8s')
     packed_data = packer.pack(*values)
     chksum =  bytes(hashlib.md5(packed_data).hexdigest(), encoding="UTF-8")
+
+
+
+
+    
     #Compare Checksums to test for corrupt data
     if UDP_Packet[3] == chksum:
         print('CheckSums Match, Packet OK')
+        resp = (1,UDP_Packet[1])
+        UDP_Data = struct.Struct('I I')
+        respData = UDP_Data.pack(*response)
+        respChksum = bytes(hashlib.md5(respData).hexdigest(), encoding="UTF-8")
+
+
+        resp = (1,UDP_Packet[1],respChksum)
+        print("Response being sent:",resp)
+        
+
+
+
+
+
+
+        
     else:
         print('Checksums Do Not Match, Packet Corrupt')
